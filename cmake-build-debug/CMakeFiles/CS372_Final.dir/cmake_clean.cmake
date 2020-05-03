82966aec76c29e7d6048764e7aa32739dbf3231a
Keegan Palmieri <fkpalmieri@alaska.edu>
2020-05-03 13:55:34 -0800

    Initial push

---
+++ cmake-build-debug/CMakeFiles/CS372_Final.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS372_Final.dir/main.cpp.o"
+  "CS372_Final"
+  "CS372_Final.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS372_Final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CS372_Final.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CS372_Final.
+# This may be replaced when dependencies are built.
