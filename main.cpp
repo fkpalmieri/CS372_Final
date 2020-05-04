6a59f931b687a81da340b24d2623e2ef36f9a2c6
Keegan Palmieri <fkpalmieri@alaska.edu>
2020-05-03 23:30:23 -0800

    add proxy example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+using std::endl;
 
+template<typename T>
+struct Property {
+    T   m_value;
+    Property(const T initialValue) { * this = initialValue; }
+    operator T() { return m_value; }
+    T operator = (T newValue) { return m_value = newValue; }
+};
+struct Creature {
+    Property<int32_t>   m_strength{10};
+    Property<int32_t>   m_agility{5};
+};
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Creature creature;
+    creature.m_agility = 20;
+    cout << creature.m_agility << endl;
+    return EXIT_SUCCESS;
 }
